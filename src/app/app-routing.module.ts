3e854ca12fc8c7737ffa6c6d2287afc2b71b834f
Jeremy-san <shehata.jeremy@gmail.com>
2021-09-09 06:01:07 +0200

    auth + header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminDashboardComponent } from './component/admin-dashboard/admin-dashboard.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { SigninComponent } from './component/signin/signin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home-page', component: HomePageComponent
+  },
+  {
+    path: 'admin/dashboard', component: AdminDashboardComponent
+  },
+  {
+    path: 'login', component: SigninComponent
+  },
+  {
+    path: '', redirectTo: 'home-page', pathMatch: 'full'
+  },
+  {
+    path: '**', redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SheetComponent } from './sheet/sheet/sheet.component';
+import { SheetComponent } from './component/sheet/sheet.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { SigninComponent } from './component/signin/signin.component';
+import { AdminDashboardComponent } from './component/admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SheetComponent
+    SheetComponent,
+    HeaderComponent,
+    HomePageComponent,
+    SigninComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,

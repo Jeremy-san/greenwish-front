6a2c8e465e1da2b17eadb4f5e326a5de9a3bfa19
Jeremy-san <shehata.jeremy@gmail.com>
2021-09-09 06:09:01 +0200

    fix auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { tap, delay } from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  isLoggedIn: boolean ; // L'utilisateur est-il connecté ?
+  redirectUrl: string; // où rediriger l'utilisateur après l'authentification ?
+  // Une méthode de connexion
+  login(name: string, password: string): Observable<boolean> {
+    // Faites votre appel à un service d'authentification...
+    const isLoggedIn = (name === 'user' && password === 'user');
+
+    return of(true).pipe(
+      delay(1000),
+      tap(val => this.isLoggedIn = isLoggedIn)
+    );
+  }
+
+  // Une méthode de déconnexion
+  logout(): void {
+    this.isLoggedIn = false;
+  }
 }
